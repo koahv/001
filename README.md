ca1fe4bd024141a3967e00c9b6645a15b8a36f07
AJOHNSON <k0OAhV@gmail.com>
2015-02-11 14:06:57 +0000

    tmux+tmux-mem-cpu-load added

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 001
 
-1 Run install.sh 
+1 Run install.sh as root (su/sudo su)
 
-2 copy .xinitrc to ~/
+2 copy .xinitrc .tmux.conf time w  to ~/
 
 3 configure display manager to run .xinitrc
--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-(cd dwm-6.0;make clean install)
-(cd st-0.5;make clean install)
+(cd dwm-6.0;		make clean install)
+(cd st-0.5;		make clean install)
+(cd tmux-1.9a;		./configure && make; make install )
+(cd tmux-mem-cpu-load	cmake .; make; make install)
