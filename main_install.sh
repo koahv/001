ef971aa37dc6bd68489454ea3a88759fde19aeb6
koahv <root@z06400.lan>
2015-02-14 14:15:29 +0000

    directory structure updated

---
+++ main_install.sh
@@ -0,0 +1,7 @@
+#links http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-hardened/
+links http://www.mirrorservice.org/sites/distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-hardened/
+
+
+
+mv stage3* /mnt/gentoo/
+
