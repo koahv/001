9cf40949e69b685cc58b55a64e4a139b71b40be0
koahv <k0OAhV@gmail.com>
2015-02-18 19:01:31 +0000

    script data

---
+++ chroot-install.sh
@@ -0,0 +1,36 @@
+echo
+echo Configure Portage
+mkdir /usr/portage
+emerge-webrsync
+
+eselect profile list
+
+read -p "Enter Number for \"hardened/linux/amd64\" " profile
+read -p "Is $profile correct?" -n 1 -r
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+eselect profile set $profile
+fi
+
+locale-gen
+
+echo KERNEL
+emerge hardened-sources genkernel grub dhcpd
+cp /001-master/config/usr/src/linux/.superkoala-8.0 /usr/src/linux/
+genkernel --menuconfig all --makeopts=-j6
+grub2-mkconfig -o /boot/grub/grub.cfg
+grub2-mkconfig -o /boot/grub/grub.cfg
+echo
+echo
+echo Set Root Password - ensure correct keymap
+passwd
+echo
+echo
+
+echo Reboot and run secondary install
+read -p "Reboot now?" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+reboot
+fi
